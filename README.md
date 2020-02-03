2a7045b5f57bef5daf02d89d80e76b69bbc63e93
David Wong <davidwg@calibra.com>
2020-02-03 11:24:01 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# repo-dispatch-bot
+
+> A GitHub App built with [Probot](https://github.com/probot/probot) that send repo dispatch to github repositories
+
---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import { Application, Context } from 'probot';
+import child_process from 'child_process';
+import { promises as fs } from 'fs';
+import { promisify } from 'util';
+import tmp from 'tmp-promise';
+
+const exec = promisify(child_process.exec);
+
+async function handle_pr(context: Context) {
+  context.log("handling PR");
+  const pr_number = context.payload.pull_request.number;
+  const head_sha = context.payload.pull_request.head.sha;
+
+  // github api lies about base sha, so we pull it from the first commit's parent
+  const commits = await context.github.pullRequests.listCommits(context.repo({ number: pr_number }));
+  const base_sha = commits.data[0].parents[0].sha;
+
+  // send a repo dispatch to calibra/mirai-bot in order to run MIRAI on the PR
+  context.github.repos.createDispatchEvent({
+    "owner": "mimoo",
+    "repo": "mirai-bot",
+    "event_type": "new_PR",
+    "client_payload": context.repo({
+      "pull_id": pr_number,
+      "pull_ref": head_sha,
+      "base_ref": base_sha
+    })
+  });
+}
+
+export = (app: Application) => {
+  app.on('pull_request.opened', async (context: Context) => {
+    context.log("pull request opened");
+    await handle_pr(context);
+  });
+
+  app.on('pull_request.synchronize', async (context: Context) => {
+    context.log("pull request synchronize");
+    await handle_pr(context);
+  });
+}
